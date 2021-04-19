eae7267fc9608b6f43db3e25ab979baf85a22d25
banikashu1989 <82701016+banikashu1989@users.noreply.github.com>
2021-04-19 09:50:34 +0530

    Add files via upload

---
+++ DAY2_19-4-2021_ashu.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+name="ashu"
+print(name)
+print(name.upper())
+print(name.title())
+
+
+# In[6]:
+
+
+print("fav_subject is math, english, biology")
+
+
+# In[8]:
+
+
+print("fav_subjectismathenglishbiology")
+
+
+# In[9]:
+
+
+print('fav_subject''\nmath''\nbiology''\nenglish')
+
+
+# In[13]:
+
+
+print('fav_subject''\n\tmath''\n\t\tbiology')
+
+
+# In[21]:
+
+
+print('my_family' '\nfather''\nmother''\n\tsister1''\t\tsister2''\t\tme''\n\tson''\t\tdaughter''\tson''\n\tdaughter')
+
+
+# In[29]:
+
+
+print('\t\t\t\t\t\t\tA''\n\t\t\t\t\t\tA''\t\tA''\n\t\t\t\t\tA''\t\t\t\tA')
+
+
+# In[34]:
+
+
+father="jagadish"
+mother="aparna"
+son="ashu"
+family=f"{father.title()} is my dad"
+print(family)
+
+
+# In[35]:
+
+
+family=f"{father.upper()} and {mother.upper()} they are {son.title()}'s parents"
+print(family)
+
+
+# In[41]:
+
+
+family1=f"{father.upper()} \n{mother.upper()}"
+print(family1)
+
+
+# In[42]:
+
+
+print("mylife")
+
+
+# In[50]:
+
+
+print('my life''\nmy rules')
+
+
+# In[54]:
+
+
+A1="hello"
+A2="every"
+A3="one"
+print(A1.title())
+print(A2.title())
+print(A3.title())
+
+
+# In[66]:
+
+
+print("\tgrandfather".upper())
+print("\t\tgrandmother".title())
+
+
+# 
