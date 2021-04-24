468755698f9282c9e90847d655ffa4e926b01a04
banikashu1989 <82701016+banikashu1989@users.noreply.github.com>
2021-04-24 10:49:48 +0530

    Add files via upload

---
+++ DAY_4_24-04-2021_ASHU.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+stud="[ashu,jaya,dodo]"
+print(stud)
+
+
+# In[6]:
+
+
+stud1=['ashu','jaya','dodo']
+print(stud1)
+
+
+# In[9]:
+
+
+print(stud[2])
+
+
+# In[18]:
+
+
+print(stud1[2],stud[4],stud1[1].upper())
+
+
+# In[20]:
+
+
+name=['rahul','sunit','arun']
+print(name)
+
+
+# In[22]:
+
+
+name.append('ashish')
+print(name)
+
+
+# In[23]:
+
+
+name.insert(1,'ashu')
+print(name)
+
+
+# In[24]:
+
+
+name.append('jalil')
+print(name)
+
+
+# In[25]:
+
+
+name.insert(2,'rohit')
+print(name)
+
+
+# In[26]:
+
+
+name
+
+
+# In[33]:
+
+
+print(name[5].upper(),name[2].title())
+
+
+# In[34]:
+
+
+type(name)
+
+
+# In[35]:
+
+
+type(stud1)
+
+
+# In[37]:
+
+
+type(stud)
+
+
+# In[38]:
+
+
+name1='(ashu,rohit,jaya,dodo)'
+print(name1)
+
+
+# In[48]:
+
+
+name2=['sam','ram','sagar']
+print(name2)
+
+
+# In[49]:
+
+
+name2.append('jaya')
+print(name2)
+
+
+# In[51]:
+
+
+name2.insert(1,'ashu')
+print(name2)
+
+
+# In[53]:
+
+
+print('grandfather','\ngrandmother','\tmother')
+
+
+# In[55]:
+
+
+a1=2
+b1=3
+a1+b1
+
+
+# In[56]:
+
+
+print(a1+b1)
+
+
+# In[57]:
+
+
+a2="5"
+b2="6"
+a2+b2
+
+
+# In[58]:
+
+
+name2
+
